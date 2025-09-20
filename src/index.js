8f6d50fa1971f6f3b3ae4fe8821cd9ea3a632720
Iris <irissagastume@unitec.edu>
2025-09-19 23:26:20 -0600

    segundo commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ app.get('/', (_req, res) => res.json({ message: 'API OK' }));
 app.use('/api/personas', personasRouter);
 
 const { MONGODB_URI, PORT = 3000 } = process.env;
-
 mongoose.connect(MONGODB_URI)
   .then(() => {
     app.listen(PORT, () => {
